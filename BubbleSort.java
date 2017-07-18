030c33464a5175c266c7a334afe1a10a1d742cce
Aleksey <javafirst17@gmail.com>
2017-07-18 14:56:29 +0300

    Create BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,76 @@
+package com.jf17.Algorithms;
+
+/**
+ * This class implements BubbleSort
+ * @author Aleksey
+ */
+ 
+public class BubbleSort {
+        
+    
+    int array[];
+    
+    int sortedArray[];
+    final  int sizeArray ;
+    boolean swap;
+    final int last;
+    int iterations;
+    
+    public BubbleSort(int arrayInt[]){
+        
+         array = arrayInt;
+         sizeArray = array.length;
+         last = sizeArray-1;
+         iterations = 0;
+         
+    }
+    
+    
+    
+    public int[] getSortedArray(){
+    
+        sort();
+    
+    return sortedArray;
+    }
+    
+     public int getIterations(){
+    
+        if(sortedArray==null){
+        sort(); return iterations;}
+        else {return iterations;}
+    
+    }
+    
+    
+    private void sort(){
+    
+        
+        		//Sorting
+		do
+     		{
+          		swap = false;
+          		for (int count = 0; count < last; count++)
+          		{
+               			if (array[count] > array[count + 1])
+               			{
+                    			int temp = array[count];
+                    			array[count] = array[count + 1];
+                    			array[count + 1] = temp;
+                    			swap = true;
+               			}
+          		}
+			
+                        
+                        iterations++;
+          		
+                    } while (swap);
+        
+        
+    
+    
+        sortedArray = array;
+    }
+    
+    
+}
