86394a4be16083e0b30aa12c32f6429bbb654ed6
Aleksey <javafirst17@gmail.com>
2017-09-14 13:18:07 +0300

    create class

---
+++ ArrayInverter.java
@@ -0,0 +1,22 @@
+package com.jf17.Algorithms;
+
+/**
+ * This class 
+ * @author Aleksey
+ */
+
+
+public class ArrayInverter {
+
+    public int[] invert(int [] data){
+
+        for(int i = 0; i<data.length/2; i++){
+
+            int tmp =data[i];
+            data[i] = data[data.length-1-i];
+            data[data.length-1-i]=tmp;
+
+        }
+            return data;
+    }
+}
